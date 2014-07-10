03661dbcb093b5aba264764c3104fc9621058f6b
fabioseverino <f.severino@alice.it>
2014-07-10 10:23:07 +0200

    classi controller

--- 10_07_14/src/controller/ModificaController.java
+++ 10_07_14/src/controller/ModificaController.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class ModificaController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class ModificaController extends AbstractController {
+
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		return new ModelAndView("modifica");
+	}
 
 }
